79e8be346cc8840365d35d27f3a92bc651e77ac6
Kurt Neaderhiser <kurt.neaderhiser@gmail.com>
2017-12-18 15:38:21 -0500

    checkpoint 1 and 2 done

---
+++ 01-introduction-to-data-structures/line/line.rb
@@ -0,0 +1,41 @@
+# A line of people at an amusement park ride
+# There is a front to the line, as well as a back.
+# People may leave the line whenever they see fit and those behind them take their place.
+class Line
+  attr_accessor :members
+
+  def initialize
+    self.members = []
+  end
+
+  def join(person)
+    self.members.push(person)
+  end
+
+  def leave(person)
+    self.members.slice!(self.members.index(person))
+  end
+
+  def front
+    self.members.first
+  end
+
+  def middle
+    self.members[((members.length / 2 ).round)]
+  end
+
+  def back
+    self.members.last
+  end
+
+  def search(person)
+    self.members.include?(person) ? person : nil;
+  end
+
+  private
+
+  def index(person)
+    self.members.index(person)
+end
+
+end
---
+++ 01-introduction-to-data-structures/screen/screen.rb
@@ -0,0 +1,29 @@
+require_relative 'pixel'
+
+class Screen
+  attr_accessor :width
+  attr_accessor :height
+  attr_accessor :matrix
+
+  def initialize(width, height)
+    self.width = width
+    self.height = height
+    self.matrix = Array.new(width, Array.new(height, nil))
+  end
+
+  # Insert a Pixel at x, y
+  def insert(pixel, x, y)
+    self.matrix[x][y] = pixel
+  end
+
+  def at(x, y)
+    inbounds(x,y) ? self.matrix[x][y] : nil
+  end
+
+  private
+
+  def inbounds(x, y)
+    ((x < width || x > 0) || (y < height || y > 0))
+end
+
+end
